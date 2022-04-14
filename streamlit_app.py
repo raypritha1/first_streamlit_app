e9ddc12aebf792903d9fdb1d06f82f3e88831dae
Pritha mazumder <103663071+raypritha1@users.noreply.github.com>
2022-04-14 09:37:17 -0700

    Update streamlit_app.py
    Create Some User Interaction
    add a user interactive widget called a Multi-select that will allow users to pick the fruits they want in their smoothies.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-import pandas
+
 
 streamlit.title('My Mom\'s New Healthy Diner')
 
@@ -11,5 +11,11 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# Lets pull a picklist here so they can pick the fruit they want to include
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+# Display the table on the page
 streamlit.dataframe(my_fruit_list)
+
+
